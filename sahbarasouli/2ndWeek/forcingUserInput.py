c2c5d2f606773077db71d102076ee3a8bf0f846a
sahba <sahbarasouli@gmail.com>
2023-12-17 17:15:07 +0330

    tasks has been uploaded!

---
+++ sahbarasouli/2ndWeek/forcingUserInput.py
@@ -0,0 +1,10 @@
+while True:
+    try:
+        inp=int(input('enter:'))
+        print(inp)
+    except ValueError:
+        print('enter again')
+    else:
+  
+        print('thank you!')
+
---
+++ sahbarasouli/2ndWeek/hob-wiz.py
@@ -0,0 +1,4 @@
+
+for number1 in range(1,101):
+    res='hobviz'if not number1 %15 else ('hob'if not number1 %5 else ('viz'if not number1 %3 else number1) )
+    print(res)
