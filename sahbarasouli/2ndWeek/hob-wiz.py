b2a6f31f65f8189294c7845de2bc670f4a854c3b
Mehdi Shokri Zadeh <31562498+Mehdi-Zadeh@users.noreply.github.com>
2023-12-24 18:39:42 +0330

    Update hob-wiz.py

--- sahbarasouli/2ndWeek/hob-wiz.py
+++ sahbarasouli/2ndWeek/hob-wiz.py
@@ -1,4 +1,5 @@
 
 for number1 in range(1,101):
-    res='hobviz'if not number1 %15 else ('hob'if not number1 %5 else ('viz'if not number1 %3 else number1) )
+    # res='hobviz'if not number1 %15 else ('hob'if not number1 %5 else ('viz'if not number1 %3 else number1) )
+    res='hopviz'if not number1 %15 else 'hob'if not number1 %3 else 'viz'if not number1 %5 else number1
     print(res)
