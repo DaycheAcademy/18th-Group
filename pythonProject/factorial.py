afee35566614e8392a155727fc878ee49064bfdf
somayeh mohri <152815598+s-mohri@users.noreply.github.com>
2024-01-14 17:15:56 +0330

    Please check my practice

---
+++ pythonProject/factorial.py
@@ -0,0 +1,11 @@
+def factor(x):
+    a = 1
+    answer = 0
+    while (a + 1) <= x:
+        answer = a * (a + 1)
+        a += 1
+    return answer
+
+
+if __name__ == '__main__':
+    print('factorial:', factor(8))
